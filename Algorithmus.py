2ada31d49b95f4a0a5ea983d120236772fcb8931
ErikSchluens <76912634+ErikSchluens@users.noreply.github.com>
2023-10-19 18:29:27 +0200

    Update Algorithmus.py
    Added use of SciPy to easily implement simplex algorithm solving

--- Algorithmus.py
+++ Algorithmus.py
@@ -1,5 +1,22 @@
-matrix = [[x1, x2, x3, x4, x5, b], 
-    [ 1, 1, 1, 0, 0, 80],
-    [1, 3, 0, 1, 0, 180],
-    [2, 1, 0, 0, 1, 120],
-    [-6, -4, 0, 0, 0, 0]]
+#Bekanntes Problem aus OR
+#6x+4y
+# x +y <=80
+# x + 3y <= 180
+# 2x+y <=120
+# x und y >= 0
+
+#importieren von der funktion aus der Bibliothek scipy
+from scipy.optimize import linprog
+
+#zielfunktion
+c = [-6, -4]
+
+#Varieblen
+A = [[1,1],[1,3],[2,1]]
+b = [80, 180, 120]
+# x und y sollen positiv sein
+bounds = [(0,None),(0,None)]
+#übergabe der Variablen in die Funktion
+res = linprog(c, A_ub=A, b_ub=b, bounds=bounds, method='highs')
+#Ausgabe des ergebnisses
+print(res)
