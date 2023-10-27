2c0f6c9a96e7350ff697c6c8af22f0168f98232e
ErikSchluens <76912634+ErikSchluens@users.noreply.github.com>
2023-10-27 08:32:12 +0200

    Update Algorithmus.py
    use better library for binary optimisation
    added first constraints for real mathematical model

--- Algorithmus.py
+++ Algorithmus.py
@@ -1,22 +1,61 @@
-#Bekanntes Problem aus OR
-#6x+4y
-# x +y <=80
-# x + 3y <= 180
-# 2x+y <=120
-# x und y >= 0
+from pulp import *
 
-#importieren von der funktion aus der Bibliothek scipy
-from scipy.optimize import linprog
+#initialize the problem
+lp = LpProblem("Stundenplan", LpMinimize)
 
-#zielfunktion
-c = [-6, -4]
+#define variables
+# Define the range for variable pairs
+range_x = range(1, 30)  # Timeslots 1 to 29
+range_y = range(1, 7)  # Kurse 1 to 6
 
-#Varieblen
-A = [[1,1],[1,3],[2,1]]
-b = [80, 180, 120]
-# x und y sollen positiv sein
-bounds = [(0,None),(0,None)]
-#übergabe der Variablen in die Funktion
-res = linprog(c, A_ub=A, b_ub=b, bounds=bounds, method='highs')
-#Ausgabe des ergebnisses
-print(res)
+# Initialize an empty list to store variable pairs
+var_keys = []
+
+# Loop through the ranges to generate variable pairs
+for x in range_x:
+    for y in range_y:
+        var_keys.append((x, y))
+x = LpVariable.dicts("Kurszeitpunkt", var_keys, lowBound=0, upBound=1, cat="Integer")
+
+# Create a dictionary to hold the PuLP variables
+variables = {}
+for x, y in var_keys:
+    variables[(x, y)] = LpVariable(f'x_{x}_{y}', cat=LpBinary)
+
+# Sum all the PuLP variables using lpSum
+sum_of_variables = lpSum(variables[(x, y)] for x, y in var_keys)
+
+#add the objective function
+lp += sum_of_variables
+
+#add the constraints
+#blocker timeslot needs to be free
+sumconstraint = LpConstraint(
+    e=variables[(4, 1)] + variables[(4, 2)] + variables[(4, 3)] + variables[(4, 4)] + variables[(4, 5)] + variables[(4, 6)],
+    sense=LpConstraintEQ,
+    name="SumConstraint",
+    rhs=0
+)
+lp +=sumconstraint
+
+#course 1 (Wissenschaft und Trends) needs to have 3 weeekly slots
+Wissenschaftundtrends = LpConstraint(
+    e=variables[(1, 1)] + variables[(2, 1)] + variables[(3, 1)] + variables[(4, 1)] + variables[(5, 1)] + variables[(6, 1)],
+    sense=LpConstraintEQ,
+    name="",
+    rhs=3
+)
+lp +=Wissenschaftundtrends
+
+
+#solve the problem
+lp.solve()
+
+# Get the values of the variables
+for x, y in var_keys:
+    print(f'x_{x}_{y} = {variables[(x, y)].varValue}')
+
+# Get the optimal objective value
+optimal_value = value(lp.objective)
+print(f'Optimal objective value = {optimal_value}')
+
