f18f35eae820ec756044432a0ec96134c5c6a7dd
HenryW1609 <110824903+HenryW1609@users.noreply.github.com>
2023-10-31 22:39:36 +0100

    Create GUI.php

---
+++ GUI.php
@@ -0,0 +1,22 @@
+<?php
+    include_once ('header.php');
+?>
+        <title>Startseite</title>
+        <h2 style="margin: 0.5%">Willkommen <?php ?></h2>
+
+        <div class="btn-group btn-group-lg">
+            <div class="text1"><a href="Stundenplan.php">+</a></div>
+            <div class="text1"><a href="#">+</a></div>
+            <div class="text2"><a href="#">+</a></div>
+        </div>
+
+            <p1 class="p1">Stundenplan erstellen</p1>
+            <p1 class="p2">Exkursionszuordnung</p1>
+            <p1 class="p3">WBÜ Zuordnung</p1>
+
+        </body>
+        <div style="height: 170px;"></div>
+
+<?php
+include_once('footer.php');
+?>
