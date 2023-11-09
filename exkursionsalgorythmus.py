383ed6aae1eb98734ffcf027cc52f9d3838f10b2
ErikSchluens <76912634+ErikSchluens@users.noreply.github.com>
2023-11-09 21:20:52 +0100

    Update exkursionsalgorythmus.py
    added connection to the database to fetch data from the tables

--- exkursionsalgorythmus.py
+++ exkursionsalgorythmus.py
@@ -1,4 +1,42 @@
 from pulp import *
+from pulp import *
+import mysql.connector
+
+#configurize the connection to the local database
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'localhost',
+  'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock',
+  'database': 'Stundenplan',
+  'raise_on_warnings': True
+}
+
+cnx = mysql.connector.connect(**config)
+
+#create a cursor that interacts with the databse
+cursor = cnx.cursor(dictionary=True)
+
+#execute a select query from the WBÜ_Input Table
+cursor.execute('SELECT * FROM `WBÜ_Input`')
+
+#fetch all the results from the query above
+results = cursor.fetchall()
+
+#display the data with a for loop
+for row in results:
+  id = row['num']
+  name = row['username']
+  wahl1 = row['wahl1']
+  wahl2 = row['wahl2']
+  wahl3 = row['wahl3']
+  wahl4 = row['wahl4']
+  wahl5 = row['wahl5']
+  print ('%s | %s | %s | %s | %s | %s | %s ' % (id, name, wahl1, wahl2, wahl3, wahl4, wahl5))
+
+#close the connection 
+cnx.close()
+
 
 #initialize the problem
 lp = LpProblem("Excursion", LpMinimize)
