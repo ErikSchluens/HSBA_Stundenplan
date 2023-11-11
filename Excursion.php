d3124983b6089e6107a313fab1b26ec42ab3c828
henrywaesch <henry.waesch@gmx.de>
2023-11-11 17:52:48 +0100

    Update 11.11.

--- Excursion.php
+++ Excursion.php
@@ -23,7 +23,6 @@
         }
     </style>
 <?php
-include_once('header.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db_host = '127.0.0.1';
@@ -50,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             VALUES ('$username', '$dropdown1', '$dropdown2', '$dropdown3', '$dropdown4', '$dropdown5')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Danke, deine Wahl wurde erfolgreich abgegeben.";
+        echo "<p style='position: absolute;margin-left: 42%;margin-top: 10%;'>" . "Danke für deine Wahl" . "</p>";
     } else {
         echo "Fehler beim Einfügen in die Datenbank: " . $conn->error;
     }
@@ -58,78 +57,82 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Datenbankverbindung schließen
     $conn->close();
 }
-var_dump($_SESSION);
 ?>
 
     <title>Exkursion</title>
     <h2 style="margin: 0.5%">Bitte wähle deine Exkursion, <?php echo $username ?></h2>
 
-        <h5 style="margin-left: 2.2%; margin-top: 5%">Verfügbare Städte</h5>
-            <table>
-                <th>Städte</th>
-                <tr><td>Hamburg</td></tr>
-                <tr><td>Lissabon</td></tr>
-                <tr><td>Athen</td></tr>
-                <tr><td>Bilbao</td></tr>
-                <tr><td>Bordeux</td></tr>
-                <tr><td>Albanien</td></tr>
-            </table>
+    <h5 style="margin-left: 2.2%; margin-top: 5%">Verfügbare Städte</h5>
+    <table>
+        <th>Städte</th>
+        <tr><td>Hamburg</td></tr>
+        <tr><td>Lissabon</td></tr>
+        <tr><td>Athen</td></tr>
+        <tr><td>Bilbao</td></tr>
+        <tr><td>Bordeux</td></tr>
+        <tr><td>Limassol</td></tr>
+    </table>
 
 
     <form method="post" style="margin-left: 10%">
 
-        <div style="margin-left: 16%; margin-top: -14%; position: absolute">1. Wahl</div>
-        <select name="dropdown1" style="position: absolute; margin-left: 15%;margin-top: -12%">
+        <div style="margin-left: 10%; margin-top: -14%; position: absolute">1. Wahl</div>
+        <select name="dropdown1" id="dropdown1" onchange="showDropdown(1)" style="position: absolute; margin-left: 10%;margin-top: -12%">
+            <option value="">Keine Stadt ausgewählt</option>
             <option value="Hamburg">Hamburg</option>
             <option value="Lissabon">Lissabon</option>
             <option value="Athen">Athen</option>
             <option value="Bilbao">Bilbao</option>
             <option value="Bordeux">Bordeux</option>
-            <option value="Albanien">Albanien</option>
+            <option value="Limassol">Limassol</option>
         </select>
 
-        <div style="margin-left: 26%; margin-top: -14%; position: absolute">2. Wahl</div>
-        <select name="dropdown2" style="position: absolute; margin-left: 25%;margin-top: -12%">
+        <div style="margin-left: 23%; margin-top: -14%; position: absolute">2. Wahl</div>
+        <select name="dropdown2" id="dropdown2" style="position: absolute; margin-left: 23%;margin-top: -12%">
+            <option value="">Keine Stadt ausgewählt</option>
             <option value="Hamburg">Hamburg</option>
             <option value="Lissabon">Lissabon</option>
             <option value="Athen">Athen</option>
             <option value="Bilbao">Bilbao</option>
             <option value="Bordeux">Bordeux</option>
-            <option value="Albanien">Albanien</option>
+            <option value="Limassol">Limassol</option>
         </select>
 
-        <div style="margin-left: 36%; margin-top: -14%; position: absolute">3. Wahl</div>
-        <select name="dropdown3" style="position: absolute; margin-left: 35%;margin-top: -12%">
+        <div style="margin-left: 35.5%; margin-top: -14%; position: absolute">3. Wahl</div>
+        <select name="dropdown3" id="dropdown3" style="position: absolute; margin-left: 35.5%;margin-top: -12%">
+            <option value="">Keine Stadt ausgewählt</option>
             <option value="Hamburg">Hamburg</option>
             <option value="Lissabon">Lissabon</option>
             <option value="Athen">Athen</option>
             <option value="Bilbao">Bilbao</option>
             <option value="Bordeux">Bordeux</option>
-            <option value="Albanien">Albanien</option>
+            <option value="Limassol">Limassol</option>
         </select>
 
-        <div style="margin-left: 46%; margin-top: -14%; position: absolute">4. Wahl</div>
-        <select name="dropdown4" style="position: absolute; margin-left: 45%;margin-top: -12%">
+        <div style="margin-left: 48%; margin-top: -14%; position: absolute">4. Wahl</div>
+        <select name="dropdown4" id="dropdown4" style="position: absolute; margin-left: 48%;margin-top: -12%">
+            <option value="">Keine Stadt ausgewählt</option>
             <option value="Hamburg">Hamburg</option>
             <option value="Lissabon">Lissabon</option>
             <option value="Athen">Athen</option>
             <option value="Bilbao">Bilbao</option>
             <option value="Bordeux">Bordeux</option>
-            <option value="Albanien">Albanien</option>
+            <option value="Limassol">Limassol</option>
         </select>
 
-        <div style="margin-left: 56%; margin-top: -14%; position: absolute">5. Wahl</div>
-        <select name="dropdown5" style="position: absolute; margin-left: 55%;margin-top: -12%">
+        <div style="margin-left: 60.5%; margin-top: -14%; position: absolute">5. Wahl</div>
+        <select name="dropdown5" id="dropdown5" style="position: absolute; margin-left: 60.5%;margin-top: -12%">
+            <option value="">Keine Stadt ausgewählt</option>
             <option value="Hamburg">Hamburg</option>
             <option value="Lissabon">Lissabon</option>
             <option value="Athen">Athen</option>
             <option value="Bilbao">Bilbao</option>
             <option value="Bordeux">Bordeux</option>
-            <option value="Albanien">Albanien</option>
+            <option value="Limassol">Limassol</option>
         </select>
 
-        <input type="submit" name="submit" class="btn btn-dark" style="background-color: #032d57; margin-top: -14%; margin-left: 75%">
+        <input type="submit" name="submit" class="btn btn-dark" style="background-color: #032d57; margin-top: -14%; margin-left: 81%">
     </form>
 
-            <div style="height: 250px; width: 100%;"></div>
+    <div style="height: 250px; width: 100%;"></div>
 <?php include_once('footer.php')?>
