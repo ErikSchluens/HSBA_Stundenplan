0f8fa47b290dc8f9407a4a9f7bbc34ab224e9c07
ErikSchluens <76912634+ErikSchluens@users.noreply.github.com>
2023-11-16 19:39:34 +0100

    Update wbüalgorithm.py

--- wbüalgorithm.py
+++ wbüalgorithm.py
@@ -17,87 +17,63 @@ cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor(dictionary=True)
 
 #execute a select query from the WBÜ_Input Table
-cursor.execute('SELECT * FROM `WBÜ_Input`')
+cursor.execute('SELECT * FROM `WBÜ_Zwischentabelle`')
 
 #fetch all the results from the query above
-results = cursor.fetchall()
+data_from_db = cursor.fetchall()
 
-#define a variable that counts the number of students
-numberofstudents = 0
-
-#display the data with a for loop
-for row in results:
-  id = row['num']
-  name = row['username']
-  wahl1 = row['wahl1']
-  wahl2 = row['wahl2']
-  wahl3 = row['wahl3']
-  wahl4 = row['wahl4']
-  wahl5 = row['wahl5']
-  print ('%s | %s | %s | %s | %s | %s | %s ' % (id, name, wahl1, wahl2, wahl3, wahl4, wahl5))
-  numberofstudents +=1 #momentan wird jeder eintrag gezählt, wir brauchen noch etwas, dass sicherstellt, dass nur ein Eintrag pro Account vorhanden ist
-
-print(numberofstudents)
-
-#initialize the problem
-lp = LpProblem("WBU", LpMinimize)
-
-#define variables
-# Define the range for variable pairs
-range_x = range(1, numberofstudents+1)  # Amount of students
-range_y = range(1, 6)  # Courses 1 bis 5
-
-# Initialize an empty list to store variable pairs
-var_keys = []
-
-# Loop through the ranges to generate variable pairs
-for x in range_x:
-    for y in range_y:
-        var_keys.append((x, y))
-x = LpVariable.dicts("Schüler", var_keys, lowBound=0, upBound=1, cat="Integer")
-
-# Create a dictionary to hold the PuLP variables
-variables = {}
-for x, y in var_keys:
-    variables[(x, y)] = LpVariable(f'x_{x}_{y}', cat=LpBinary)
-
-# Sum all the PuLP variables using lpSum
-sum_of_variables = lpSum(variables[(x, y)] for x, y in var_keys)
-
-#add the objective function
-lp += sum_of_variables
-
-#add the constraints
-#only one course per timeslot
-
-for i in range_x:
-    i = LpConstraint(
-        e=lpSum(variables[(i, y)] for y in range_y),
-        sense=LpConstraintEQ,
-        name=i,
-        rhs=1)
-    lp += i
+#delete old data from output table
+cursor.execute('Delete FROM `WBÜ_Output`')
+cnx.commit()
 
 
-#solve the problem
-lp.solve(PULP_CBC_CMD(msg=0))
+for row in data_from_db:
+    print(row)
 
-# Get the values of the variables
-for x, y in var_keys:
-    print(f'x_{x}_{y} = {variables[(x, y)].varValue}')
-    '''
+#transform data into a list - muss das sein? Kann ich nicht auch direkt das dictionary nehmen?
+results = [[row['num'], row['username'], row['Spanisch'], row['kommunikation'], row['Verhandlungsführung'],
+            row['Selfempowerment'], row['presentation_skills']] for row in data_from_db]
+
+results_dict = {index: row[0] for index, row in enumerate(results)}
+
+# Create a PuLP minimization problem
+prob = LpProblem("OptimizationProblem", LpMinimize)
+
+# Create decision variables Xij
+rows = len(results)
+cols = len(results[0]) - 2  # Exclude the first two columns
+X = [[LpVariable(f"X{i}{j+1}", lowBound=0, cat="Binary") for j in range(cols)] for i in results_dict.values()]
+                        #results[i][0]}
+
+# Create the objective function
+objective_function = lpSum(results[i][j + 2] * X[i][j] for i in range(rows) for j in range(cols))
+prob += objective_function
+
+# Now add all the constraints
+#Only 2 students in each course
+for i in range(rows):
+    prob += lpSum(X[i][j] for j in range(cols)) == 2, f"Row_{i+1}_constraint"
+
+#every student needs to have 2 courses
+for j in range(cols):
+    prob += lpSum(X[i][j] for i in range(rows)) <= 2, f"Coulumn_{j+1}_constraint"
+
+# Solve the problem
+prob.solve()
+
+# Print the results
+print("Status:", prob.status)
+print("Optimal values:")
+for var in prob.variables():
+    print(f"{var.name} = {var.varValue}")
     insertion = "INSERT INTO `WBÜ_Output` (variable, wert) VALUES (%s, %s)"
-    data = (f'x_{x}_{y}', f'{variables[(x, y)].varValue}', )
+    data = (f'{var.name}', f'{var.varValue}', )
     cursor.execute(insertion, data)
-    '''
 
-# Get the optimal objective value
-optimal_value = value(lp.objective)
-print(f'Optimal objective value = {optimal_value}')
+print("Optimal Objective Function Value:", prob.objective.value())
 
-# Use a parameterized query to insert the otimal_value into the database
-insert_query = "INSERT INTO `WBÜ_Output` (variable, wert) VALUES (%s, %s)"
-data_to_insert = ("Gesamtanzahl", optimal_value,)
+
+
 
 # Execute the query
 #cursor.execute(insert_query, data_to_insert)
