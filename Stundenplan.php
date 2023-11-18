fe66d411250ae57e38a812e954fba299afca9ce0
hannaschmuck <hannaschmuck123@gmail.com>
2023-11-18 15:55:42 +0100

    Start überarbeitung Stundenplan-Seite

--- Stundenplan.php
+++ Stundenplan.php
@@ -1,19 +1,50 @@
 <?php include_once('header.php')?>
 
-        <title>Stundenplan</title>
-        <h2 style="margin: 0.5%">Stundenplan erstellen</h2>
+    <title>Stundenplan</title>
+    <h2 style="margin: 0.5%">Stundenplan erstellen:</h2>
 
-                <div style="position: absolute; left: 5%; top: 25%;"><h5>Kursname</h5>
-                    <input type="text" placeholder="Kursname eingeben">
-                </div>
+    <div class="homepage_optioncontainer">
 
-                <div style="position: absolute; left: 43%; top: 25%;"><h5>Kurszusammensetzung</h5>
-                    <input type="text" placeholder="Auswählen">
-                </div>
+        <div>
+            <h5>Modulname</h5>
+            <div class="tooltip-container" style="margin-left: 95%; margin-top: -15.5%;">
+                <img src='Info.png' alt='Info-Icon' style="width: 20px; height: 30px;">
+                <span class="tooltip">Bitte gib dem Modul einen Namen z.B. "Mathe-BI1".</span>
+            </div>
+            <div>
+                <input type="text" style="height: 38px;" placeholder="Kursname eingeben">
+                <button type="submit" name="submit" class="btn btn-dark" style="background-color: #032d57; margin-bottom: 1.1%">Bestätigen</button>
+            </div>
+        </div>
 
-                <div style="position: absolute; left: 75%; top: 25%;"><h5>Einzelne Personen</h5>
-                    <input type="text" placeholder="Suchen">
-                </div>
+        <div>
+            <h5>Kurszusammensetzung</h5>
+
+            <div class="tooltip-container" style="margin-left: 105%; margin-top: -15.5%;">
+                <img src='Info.png' alt='Info-Icon' style="width: 20px; height: 30px;">
+                <span class="tooltip">Bitte wähle mehrere Kurse aus, die an dem Modul teilnehmen sollen.</span>
+            </div>
+            <div style="margin-top: 4%">
+                <select name="dropdown1" id="dropdown1" onchange="showDropdown(1)">
+                    <option value="">Keinen Kurs</option>
+                    <option value="22A-BA1">22A-BA1</option>
+                    <option value="22A-BA2">22A-BA2</option>
+                    <option value="22A-BA3">22A-BA3</option>
+                    <option value="22A-BA4">22A-BA4</option>
+                    <option value="22A-BI1">22A-BI1</option>
+                </select>
+                <select name="dropdown2" id="dropdown2">
+                    <option value="">Keinen Kurs</option>
+                    <option value="22A-BA1">22A-BA1</option>
+                    <option value="22A-BA2">22A-BA2</option>
+                    <option value="22A-BA3">22A-BA3</option>
+                    <option value="22A-BA4">22A-BA4</option>
+                    <option value="22A-BI1">22A-BI1</option>
+                </select>
+            </div>
+            <button type="submit" name="submit" class="btn btn-dark" style="background-color: #032d57; margin-top: 8%">Bestätigen</button>
+        </div>
+    </div>
 
     <div style="height: 500px; width: 100%;"></div>
 <?php include_once('footer.php')?>
