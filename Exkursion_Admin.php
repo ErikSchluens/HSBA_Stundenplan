d1d62627ad09fbda33195ed3782c8b05011ed74a
Erik <erik.schluens@myhsba.de>
2023-11-21 16:35:32 +0100

    Add numbers for steps for better UX

--- Exkursion_Admin.php
+++ Exkursion_Admin.php
@@ -148,57 +148,58 @@ if ($mysqli->connect_error) {
 
     <!-- Nach dem man sieht wie viele Wahlen es gibt, kann man die Anzahl der Studenten pro Exkursion festelegen -->
     <div class="homepage_optioncontainer" style="margin-top: 2%">
-        <div style="align-items: center; justify-content: center; text-align: center;>
+        <h5> 1. Gib die maximale Anzahl der Studenten pro Exkursion an</h5>
+        <div style="align-items: center; justify-content: center; text-align: center;">
             <form method="post">
-        <label for="number">Maximal-Anzahl der Studenten pro Exkursion:</label>
-        <input type="number" id="numberofstudents" name="numberofstudents" required min="1"> <!--min gibt kleinste Zahl an. -->
-        <input type="submit" value="Submit">
-        </form>
+                <label for="number">Maximal-Anzahl der Studenten pro Exkursion:</label>
+                <input type="number" id="numberofstudents" name="numberofstudents" required min="1"> <!--min gibt kleinste Zahl an. -->
+                <input type="submit" value="Submit">
+            </form>
+        </div>
     </div>
-</div>
 
-<!-- Mit diesem Button kann man die Python scripte starten-->
-<div class="homepage_optioncontainer" style="margin-top: 2%">
-    <div style="align-items: center; justify-content: center; text-align: center;">
-        <h5> Sample Text, um Exkursions Verteilung zu starten - wuh! </h5>
-        <div> Yeah yeah yeah </div>
-        <button type="submit" name="submit" class="btn btn-dark" style="background-color: #032d57; margin-top: 2%"> Start</button>
+    <!-- Mit diesem Button kann man die Python scripte starten-->
+    <div class="homepage_optioncontainer" style="margin-top: 2%">
+        <div style="align-items: center; justify-content: center; text-align: center;">
+            <h5> 2. Klicke auf diesen Button, um die Python Scripte zu starten  </h5>
+            <div> Yeah yeah yeah </div>
+            <button type="submit" name="submit" class="btn btn-dark" style="background-color: #032d57; margin-top: 2%"> Start</button>
+        </div>
     </div>
-</div>
 
-<div style="margin-left: 7%; margin-top: 2%; margin-bottom: 2%;">
-    <h5> Exkursions-Zuteilung
-    </h5>
-    <div> In der untenstehenden Tabelle ist die optimale Zuteilung für die vorliegenden Exkursionen zu sehen.
+    <div style="margin-left: 7%; margin-top: 2%; margin-bottom: 2%;">
+        <h5> Exkursions-Zuteilung
+        </h5>
+        <div> In der untenstehenden Tabelle ist die optimale Zuteilung für die vorliegenden Exkursionen zu sehen.
+        </div>
     </div>
-</div>
-<div class="homepage_optioncontainer">
-    <table style="width:90%">
-        <tr style="background-color: rgba(3, 45, 87, 0.27);">
-            <td class="table_cell">
-                Variable
-            </td>
-            <td class="table_cell">
-                WBÜ
-            </td>
-        </tr>
-        <!-- Für jede vorhande Wahl wird eine Tabellezeile ausgegeben -->
-        <?php
-        // SQL-Abfrage für Daten ausführen
-        $dataQuery = "SELECT * FROM [Tabellenname]";
-        $dataResult = $mysqli->query($dataQuery);
-        foreach($dataResult as $row) {
-            ?>
-            <!-- In die Tabellenzeilen werden die Inhalte aus dem SQL übernommen -->
-            <tr>
-                <td class="table_cell"><?=$row['variable']?></td>
-                <td class="table_cell"><?=$row['wert']?></td>
+    <div class="homepage_optioncontainer">
+        <table style="width:90%">
+            <tr style="background-color: rgba(3, 45, 87, 0.27);">
+                <td class="table_cell">
+                    Variable
+                </td>
+                <td class="table_cell">
+                    WBÜ
+                </td>
             </tr>
+            <!-- Für jede vorhande Wahl wird eine Tabellezeile ausgegeben -->
             <?php
-        }
-        ?>
-    </table>
-</div>
+            // SQL-Abfrage für Daten ausführen
+            $dataQuery = "SELECT * FROM [Tabellenname]";
+            $dataResult = $mysqli->query($dataQuery);
+            foreach($dataResult as $row) {
+                ?>
+                <!-- In die Tabellenzeilen werden die Inhalte aus dem SQL übernommen -->
+                <tr>
+                    <td class="table_cell"><?=$row['variable']?></td>
+                    <td class="table_cell"><?=$row['wert']?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+    </div>
 
 </div>
 <?php
