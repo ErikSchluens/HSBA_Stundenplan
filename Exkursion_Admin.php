032466d15a54896015ac54ce8bf5883bd216f31d
hannaschmuck <hannaschmuck123@gmail.com>
2023-11-26 12:56:52 +0100

    Outsourcing the database connection to the header.

--- Exkursion_Admin.php
+++ Exkursion_Admin.php
@@ -9,22 +9,6 @@ if ($_SESSION['user_id'] >=24 || in_array($_SESSION['user_id'], $forbiddenrange)
     header('Location:Gui.php');
 }
 ?>
-
-<?php
-// Connection to DB
-$db_host = '127.0.0.1';
-$db_user = 'root';
-$db_password = 'root';
-$db_db = 'Stundenplan'; // Hier sollte der Name deiner Datenbank stehen
-$db_port = 8889;
-
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_db, $db_port);
-
-if ($mysqli->connect_error) {
-    die("Verbindung zur Datenbank fehlgeschlagen: " . $mysqli->connect_error);
-}
-?>
-
 <div class="body_box">
 
     <h2 style="margin: 0.5%" >Hallo <?php echo $username ?>!</h2>
